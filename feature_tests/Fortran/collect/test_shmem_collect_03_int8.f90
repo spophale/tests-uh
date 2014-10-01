b6ef25aacac6735a8bbdf4e3ac1b9a60c7c695e2
Swaroop Pophale <swaroop@hpchead.mtr.labs.mlnx>
2014-10-02 00:47:45 +0300

    Corrected incorrect length arguments to collect. Also, 'target' is a keyword in Fortan and should not be used as a variable

--- feature_tests/Fortran/collect/test_shmem_collect_03_int8.f90
+++ feature_tests/Fortran/collect/test_shmem_collect_03_int8.f90
@@ -38,37 +38,39 @@ program test_shmem_collects
   implicit none
   include 'shmem.fh'
  
-
-  integer,        save :: pSync(SHMEM_COLLECT_SYNC_SIZE)
-
-  integer,   parameter :: min_npes = 2
-  integer,   parameter :: nelems = 10 
-  integer,   parameter :: target_nelems = nelems * min_npes ! assuming 2 pes ( 2 x 4 elements)
-
-  integer*8             :: src(nelems)
-  integer*8            :: src_addr
-  pointer              (src_addr, src)
-
-  integer*8             :: target(target_nelems)
-  integer*8            :: target_addr
-  pointer              (target_addr, target)
-
-  integer*8             :: target_expected(target_nelems)
-
-  integer, save        :: flag
-  integer              :: npes, me
-  integer              :: i, pe, k, tmp
-  logical              :: success
-  integer              :: collect_nelems
-  integer              :: errcode, abort
+  integer              :: npes
 
 ! Function definitions
   integer              :: my_pe, num_pes
   
-
   call start_pes(0)
-
   npes = num_pes()
+  call sub1(npes)
+
+end program test_shmem_collects
+
+subroutine sub1(npes)
+  implicit none
+  include 'shmem.fh'
+
+  integer  ::  me
+  integer,        save :: pSync(SHMEM_COLLECT_SYNC_SIZE)
+
+  integer*8             :: src
+  integer*8            :: src_addr
+  pointer              (src_addr, src)
+
+  integer*8             :: dest(npes)
+  integer*8            :: dest_addr
+  pointer              (dest_addr, dest)
+
+  integer*8             :: dest_expected(npes)
+
+  integer, save        :: flag
+  integer              :: i
+  logical              :: success
+  integer              :: errcode, abort
+
   me   = my_pe()
 
   pSync(:) = SHMEM_SYNC_VALUE
@@ -78,60 +80,33 @@ program test_shmem_collects
     success = .TRUE.
     flag = 0
 
-    call shpalloc (target_addr, target_nelems, errcode, abort)
-    call shpalloc(src_addr, nelems, errcode, abort)
+    call shpalloc (dest_addr, npes, errcode, abort)
+    call shpalloc(src_addr, 1, errcode, abort) !every PE contributes one element
 
-! The number of elements to collect from each PE
-    collect_nelems = nelems / npes
 
-    do i = 1, target_nelems, 1
-      target(i) = -9
-      target_expected = -9
+    do i = 1,npes,1
+      dest(i) = -9
+      dest_expected = -9
     end do
 
-    do i = 1, nelems, 1
-      src(i) = i * 100 + me
-    end do
+    src =  101 + me     !0 contributes 101, 1 contributes 102..
     
-    k = 1
-    do pe = 0, npes - 1, 1
-     if(mod(pe, 2) == 0) then
-       tmp = collect_nelems + 1
-     else
-       tmp = collect_nelems
-     end if
-      do i = 1, collect_nelems, 1
-        target_expected(k) = i * 100 + pe  
-        k = k + 1
-      end do
-    end do
-   
-     if(mod(pe, 2) == 0) then
-       collect_nelems = collect_nelems + 1
-     end if
- 
     call shmem_barrier_all()
 
-    call shmem_collect64(target, src, collect_nelems, &
-      0, 0, npes, &
+    call shmem_collect64(dest, src,1,0, 0, npes, &
       pSync)
 
-    do i = 1, collect_nelems * npes, 1
-      if(target(i) .ne. target_expected(i)) then
-        if(me .ne. 0) then
+
+    do i = 1, npes, 1
+      if(dest(i) .ne. (100+i)) then
           call shmem_int4_inc(flag, 0)
-        end if
       end if
     end do
 
     call shmem_barrier_all()
 
     if(me .eq. 0) then
-      if(flag .ne. 0) then
-        success = .FALSE.
-      end if
-
-      if(success .eqv. .TRUE.) then
+      if(flag .eq. 0) then
         write(*,*) "Test shmem_collect32: Passed"
       else
         write(*,*) "Test shmem_collect32: Failed"
@@ -140,11 +115,13 @@ program test_shmem_collects
 
     call shmem_barrier_all()
 
-    call shpdeallc (target_addr, errcode, abort)
+    call shpdeallc (dest_addr, errcode, abort)
     call shpdeallc(src_addr, errcode, abort)
 
   else
     write (*,*) "This test requires ", min_npes, " or more PEs." 
   end if
 
-end program test_shmem_collects
+end subroutine sub1
+
+
