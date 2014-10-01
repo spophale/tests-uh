a49b504cc46dc8641ec1951fc14b3da95b502e80
Swaroop Pophale <swaroop@hpchead.mtr.labs.mlnx>
2014-10-01 20:32:55 +0300

    Correcting error messages, making barrier code executable when used alone (without perl script).

--- feature_tests/Fortran/barrier/test_shmem_barrier_all_03.f90
+++ feature_tests/Fortran/barrier/test_shmem_barrier_all_03.f90
@@ -33,12 +33,17 @@
 ! NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 ! SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 !
-!
+!SP: This test if used 'standalone' will hang.
 
 program test_shmem_barrier
   implicit none
 
   include 'shmem.fh'
+  integer*4                :: dest
+  integer*8               :: dest_ptr
+  pointer                 (dest_ptr, dest)
+
+  integer*4                :: src
 
   integer, save :: flag
   integer       :: me, npes, i
@@ -52,6 +57,9 @@ program test_shmem_barrier
 
   me   = my_pe();
   npes = num_pes();
+  dest = 0
+  
+  call shpalloc(dest_ptr, 1, errcode, abort)
 
   if (npes .gt. 1) then
     
@@ -60,8 +68,20 @@ program test_shmem_barrier
     end if
 
 ! All PEs should call shmem_barrier_all before leaving the barrier
-    if(me .eq. npes - 1) then
-      write (*,*) 'Test shmem_barrier_all: Failed'
+! Atomic increment of destination on 0
+
+    call shmem_int4_inc(dest, 0)
+
+    if(me .eq. 0) then
+      if(dest .eq. npes - 1) then
+        write (*,*) 'Test shmem_barrier_all: Passed'
+      else
+        write (*,*) 'Test shmem_barrier_all: Failed'
+      end if
+    end if
+
+    if(me .ne. npes - 1) then
+      call shmem_barrier_all()
     end if
 
   else
